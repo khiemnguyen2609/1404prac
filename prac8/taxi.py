b7242181eb4a99b5a7998519645ddfd85475b74c
khiemnguyen2609 <54664372+khiemnguyen2609@users.noreply.github.com>
2019-10-09 14:52:21 +1000

    a

--- prac8/taxi.py
+++ prac8/taxi.py
@@ -1,17 +1,19 @@
 """
 CP1404/CP5632 Practical
-Car class
+Taxi class, derived from Car
 """
-from prac8.car import Car
+
+from car import Car
 
 
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
 
-    def __init__(self, name, fuel, price_per_km):
+    price_per_km = 1.23
+
+    def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
-        self.price_per_km = price_per_km
         self.current_fare_distance = 0
 
     def __str__(self):
@@ -22,7 +24,9 @@ class Taxi(Car):
 
     def get_fare(self):
         """Return the price for the taxi trip."""
-        return self.price_per_km * self.current_fare_distance
+        # round fare result to nearest 10c
+        fare = round(self.price_per_km * self.current_fare_distance, 1)
+        return fare
 
     def start_fare(self):
         """Begin a new fare."""
